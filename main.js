b003b93856833516cc7be22a644a2b2e6a018c09
Sayed <sayed.sais@gmail.com>
2024-08-09 13:27:34 +0200

    last last commit

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
 //Please read instructions in PROJECT_PRESENTATION.md 
 
 export function sortParenthesis (input) {
-    let openCount = 0;
-    let closeCount = 0;
-    let result = '';
-
-    // First pass: count matching pairs
-    for (let char of input) {
-        if (char === '(') {
-            openCount++;
-        } else if (char === ')') {
-            if (openCount > 0) {
-                openCount--;
-                result += '()';
-            } else {
-                closeCount++;
-            }
-        }
-    }
-
-    // Add unmatched parentheses at the end
-    result += '('.repeat(openCount);
-    result += ')'.repeat(closeCount);
-
-    return result;
-
 };
 
 export function bonusChallange (input) {
